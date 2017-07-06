9899c6cbae7b2e4d4a5d2608af380db165a5526d
Vladimir Ananiev <vladeto97@gmail.com>
2017-07-06 13:30:46 +0300

    Update installSpotify.sh

--- installSpotify.sh
+++ installSpotify.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}Installing Spotify ${NC}"
 sudo dnf config-manager --add-repo=http://negativo17.org/repos/fedora-spotify.repo
 sudo dnf install spotify
