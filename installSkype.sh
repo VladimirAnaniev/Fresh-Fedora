b2b198f170cc8f7a1aa458e7a8765aa0043a333e
Vladimir Ananiev <vladeto97@gmail.com>
2017-07-06 14:05:16 +0300

    Create installSkype.sh

---
+++ installSkype.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}Downloading Skype ${NC}"
+mkdir temp && cd ./temp
+wget -O skype.rpm https://go.skype.com/skypeforlinux-64.rpm
+
+echo -e "${GREEN} Installing Skype ${NC}"
+sudo rpm -i hyper.rpm
+
+echo -e "${GREEN} Cleaning up ${NC}"
+cd .. && rm -rf temp
