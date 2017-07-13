896555fa935cdfd3be2073ca0ed2bcc5128b26d3
Vladimir Ananiev <vladeto97@gmail.com>
2017-07-13 21:51:19 +0300

    Added Trim to initial script and animated backgrounds

---
+++ Appearance/F26 Animated Backgrounds.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}Installing Animated Backgrounds ${NC}"
+sudo dnf install f26-backgrounds-animated
--- Essentials/Initial Configurations and Installations.sh
+++ Essentials/Initial Configurations and Installations.sh
@@ -3,14 +3,17 @@
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-echo -e "${GREEN}Downloading Chrome ${NC}"
-mkdir temp && cd ./temp
-wget -O chrome.rpm https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+echo -e "${GREEN}Updating System ${NC}"
+sudo dnf update
 
-echo -e "${GREEN} Installing Chrome ${NC}"
-sudo rpm -i chrome.rpm
+echo -e "${GREEN}Enable TRIM ${NC}"
+sudo systemctl enable fstrim.timer
 
-echo -e "${GREEN} Cleaning up ${NC}"
-cd .. && rm -rf temp
+echo -e "${GREEN}Installing RPMFusion ${NC}"
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-echo -e "${GREEN} All Done! ${NC}"
+echo -e "${GREEN}Installing Gnome Tweak Tool ${NC}"
+sudo dnf install gnome-tweak-tool
+
+echo -e "${GREEN}Installing Unzip ${NC}"
+sudo dnf install unzip
